3362b44c98be58c9871dfdd7eb2f38182500977e
hustleCoderAnkur <kumawatankur48@gmail.com>
2026-06-20 11:34:11 +0530

    added google Scopes in the project

--- src/lib/googleScopes.ts
+++ src/lib/googleScopes.ts
@@ -2,4 +2,11 @@ export const GOOGLE_SCOPES = [
     "openid",
     "email",
     "profile",
+
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.labels",
+
+    "https://www.googleapis.com/auth/calendar",
 ];
